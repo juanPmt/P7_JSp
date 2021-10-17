84820d50cb813987a6551555e5ccf144a43058b8
Laura Camila Manchola Cocuy <laura.manchola.mt@correo.usa.edu.co>
2021-10-17 17:07:45 -0500

    base del proyecto

---
+++ src/test/java/co/usa/edu/grupo0/ciclo3/Ciclo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.usa.edu.grupo0.ciclo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ciclo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
